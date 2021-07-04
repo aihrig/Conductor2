ffc637f3f054627676484dcec97486fd6fcdf249
Arlo Ihrig <aihrig@users.noreply.github.com>
2021-07-04 00:18:24 -0700

    Rename build folder, remove from repo

--- build/core/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-d:\Development\MyProjects\BrickCity\Conductor2\build\core\libb64\cencode.c.o: \
- C:\Users\aihrig\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cencode.c \
- C:\Users\aihrig\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cencode.h
--- build/libraries/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-d:\Development\MyProjects\BrickCity\Conductor2\build\libraries\WebServer\detail\mimetable.cpp.o: \
- C:\Users\aihrig\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6\libraries\WebServer\src\detail\mimetable.cpp \
- C:\Users\aihrig\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6\libraries\WebServer\src\detail\mimetable.h \
- C:\Users\aihrig\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6\cores\esp32/pgmspace.h
