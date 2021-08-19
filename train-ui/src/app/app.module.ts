087a84ac04c6238d593197ac9208411b9a9ea1d8
Arlo Ihrig <aihrig@users.noreply.github.com>
2021-08-19 15:04:05 -0700

    Initial train-ui commit

---
+++ train-ui/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SpeedometerComponent } from './controls/speedometer/speedometer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SpeedometerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
