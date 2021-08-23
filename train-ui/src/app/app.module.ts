32344ecd522c0808d01f2d4194b36b9774a7efc0
Arlo Ihrig <aihrig@users.noreply.github.com>
2021-08-22 23:10:03 -0700

    Real-time telemetry service and speedometer gauge working

--- train-ui/src/app/app.module.ts
+++ train-ui/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SpeedometerComponent } from './controls/speedometer/speedometer.component';
+
+// import * as echarts from 'echarts';
 import { NgxEchartsModule } from 'ngx-echarts';
+
 import { ButtonsComponent } from './controls/buttons/buttons.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TelemetryService } from './services/telemetry.service';
+import { PubNubAngular } from 'pubnub-angular2';
 @NgModule({
     declarations: [AppComponent, SpeedometerComponent, ButtonsComponent],
     imports: [
@@ -22,7 +26,7 @@ import { TelemetryService } from './services/telemetry.service';
         FontAwesomeModule,
         NgbModule,
     ],
-    providers: [TelemetryService],
+    providers: [TelemetryService, PubNubAngular],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
